bc2c80b45f5592faf9b9f50def35315be5b0b977
GuruOrGoru <siddharthadhakall3722@gmail.com>
2025-10-04 20:55:46 +0545

    Update lexer test: Add proper syntax

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,19 +7,60 @@ import (
 )
 
 func TestNextToken(T *testing.T) {
-	input := `+=(){};,`
+	input := `
+	manau a = 5;
+	manau b = 10;
+	manau add = karya(x, y) {
+		x + y;
+	}
+
+	manau result = add(a, b)
+	`
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
-		{token.PLUS, "+"},
+		// manau a = 5;
+		{token.LET, "manau"},
+		{token.IDENTIFIER, "a"},
 		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		// manau b = 10;
+		{token.LET, "manau"},
+		{token.IDENTIFIER, "b"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		// manau add = karya(x, y) { x + y; }
+		{token.LET, "manau"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "karya"},
 		{token.LEFTPARENTHESIS, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
 		{token.RIGHTPARENTHESIS, ")"},
 		{token.LEFTBRACES, "{"},
-		{token.RIGHTBRACES, "}"},
+		{token.IDENTIFIER, "x"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "y"},
 		{token.SEMICOLON, ";"},
+		{token.RIGHTBRACES, "}"},
+
+		// manau result = add(a, b)
+		{token.LET, "manau"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENTIFIER, "add"},
+		{token.LEFTPARENTHESIS, "("},
+		{token.IDENTIFIER, "a"},
 		{token.COMMA, ","},
+		{token.IDENTIFIER, "b"},
+		{token.RIGHTPARENTHESIS, ")"},
 		{token.EOF, ""},
 	}
 
